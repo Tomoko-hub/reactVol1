73cae567d2ed05ab8a166a4249e4905de4f62382
Tomoko <haisaitomoco@gmail.com>
2021-09-06 13:05:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import  { useState } from 'react';
+import { List } from "./List"
 
 function App() {
+  const [description, setDescription] = useState('Before Click');
+  const changeDescription = () => {
+    setDescription('After Click');
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { description }
+      <List title="This is list title"/>
+      <button onClick={changeDescription}>Button</button>
     </div>
+    
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,16 @@
+export const List = ({ title }) => {
+    
+    return(
+        <div>
+            <h4>
+                { title }
+            </h4>
+            <div>
+                This is List
+            </div>
+        </div>
+    )
+
+}
+
+    
